321b50d407d5ec4f0b660ce672e37402625cd87d
Jordan Winslow <admin@jordanwinslow.me>
2019-08-10 02:04:45 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -12,6 +12,6 @@ function getIt() {
 
 function frameIt() {
   $("img").on("load", function(e){
-    document.querySelector("img").classList.toggle("tasty")
+    $("img").classList.add("tasty")
   })
 }
